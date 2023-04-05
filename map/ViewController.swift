2a65ac2cebf770777dd27dfea0f7132c3f0fe2c2
irfanizudin <irfanizzudyn@gmail.com>
2023-04-05 10:24:22 +0700

    Initial Commit

---
+++ map/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  map
+//
+//  Created by Irfan Izudin on 05/04/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
